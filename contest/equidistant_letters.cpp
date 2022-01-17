93c3ac55723889045180cce7a57d502785193f5a
Nilesh <nile.nil2@gmail.com>
2022-01-17 14:05:15 +0530

    rotate array

---
+++ contest/equidistant_letters.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    cout<<s;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+}
